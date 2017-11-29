88c503cae7f8f2dd44ffef4505a702958c62214e
jwats287 <jwats287@users.noreply.github.com>
2017-11-29 18:44:39 -0500

    Server up and running

---
+++ server.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const app = express();
+const UsersController = require('./controllers/user');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI);
+
+const connection = mongoose.connection;
+connection.on('connected', () => {
+  console.log('Mongoose Connected Successfully');    
+}); 
+
+// If the connection throws an error
+connection.on('error', (err) => {  
+  console.log('Mongoose default connection error: ' + err);
+}); 
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {  
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+  });
+
+
+
+
+app.get('/', (req,res) => {
+  res.send('Hello world!')
+})
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log("Magic happening on port " + PORT);
+})
